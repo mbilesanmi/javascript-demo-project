7df70e476cf7fb95c9cd1be9f02f83eaa362df87
Maranatha A. Ilesanmi <maranatha.ilesanmi@gmail.com>
2017-03-10 16:02:55 +0100

    Expect Test cases introduced here

--- app/library.js
+++ app/library.js
@@ -4,25 +4,35 @@ module.exports = {
 
  /* Find the average of three integers */
  computeAverage: function(num1, num2, num3) {
-   var average;
-   return average;
+     var average;
+     var sum = num1 + num2 + num3;
+     var average = sum/3;
+     return average;
  },
  
  /* Find the factorial of an integer */
  computeFactorial: function(num) {
-   var factorial;
+   var factorial = num;
+    if (num === 0 || num === 1)
+     return 1;
+    while (num > 1) {
+     num--;
+     factorial *= num;
+    }
    return factorial;
  },
 
  /* Convert a given Celcius temperature to Fahrenheit */
  convertTempCtoF: function(cTemp) {
-   var fTemp;
+   var fTemp = 0;
+     fTemp = cTemp * 9 / 5 + 32;
    return fTemp;
  },
 
  /* Convert a given Fahrenheit temperature to Celcius */
  convertTempFtoC: function(fTemp) {
-   var cTemp;
+   var cTemp = 0;
+     cTemp = (fTemp - 32) * 5 / 9;
    return cTemp;
  }
 
--- tests/tests.js
+++ tests/tests.js
@@ -4,15 +4,17 @@
 
 var chai = require('chai');
 var assert = chai.assert;
+var expect = chai.expect;
+chai.should();
 
 var myApp = require('../app/library.js');
 
 describe("Arithmetic Functionality", function() {
   it("should return 2 as average for 1, 2, 3", function() {
-    assert(myApp.computeAverage(1,2,3) == 2);
+    expect(myApp.computeAverage(1,2,3)).to.equal(2);
   })
   it("should return 5 as average for 3, 7, 5", function() {
-    assert(myApp.computeAverage(3, 7, 5) == 5);
+    expect(myApp.computeAverage(3, 7, 5)).to.equal(5);
   })
   it("should return 120 as factorial for 5", function() {
     assert(myApp.computeFactorial(5) == 120);
@@ -21,16 +23,16 @@ describe("Arithmetic Functionality", function() {
 
 
 describe("Temperature Conversion Functionality", function() {
-  it("should return X for Celcius value Y", function() {
-    assert(myApp.convertTempCtoF(40) == 104);
+  it("should return 104 for Celcius value 40", function() {
+    myApp.convertTempCtoF(40).should.equal(104);
   })
-  it("should return X for Celcius value Y", function() {
+  it("should return 140 for Celcius value 60", function() {
     assert(myApp.convertTempCtoF(60) == 140);
   })
-  it("should return Y for Fahrenheit value X", function() {
+  it("should return 60 for Fahrenheit value 140", function() {
     assert(myApp.convertTempFtoC(140) == 60);
   })
-  it("should return Y for Fahrenheit value X", function() {
+  it("should return 40 for Fahrenheit value 104", function() {
     assert(myApp.convertTempFtoC(104) == 40);
   })
 })
